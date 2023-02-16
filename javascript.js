e158fff6855ae9182ddf50ae93489314a648525b
Wilhelm Lidblad Jerrestål <116280644+wi1helm@users.noreply.github.com>
2023-02-16 19:58:10 +0100

    fixed some js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,25 @@
 
-//const img1 = {"id":"anglewar","title":"Angle War 4k","desc":"Obama","price":"100"}
+let products = [{id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
+                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
+                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
+                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
+                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
 
-function changeImage(){
-    document.getElementById("img1").src="../images/new/angle war.png";
-    console.log("obama");
+]
+
+
+function saveClick(id){
+    console.log(id);
+    sessionStorage.setItem("lastClick", id);
+}
+
+function load(){
+    lastclick = sessionStorage.getItem("lastClick");
+    console.log(lastclick);
+    document.getElementById("title").innerHTML = products.find(x => x.id === lastclick).title;
+    document.getElementById("theme").innerHTML = products.find(x => x.id === lastclick).theme;
+    document.getElementById("desc").innerHTML = products.find(x => x.id === lastclick).decs;
+    document.getElementById("price").innerHTML = products.find(x => x.id === lastclick).price;
+    document.getElementById("img").src = products.find(x => x.id === lastclick).img;
     
 }
