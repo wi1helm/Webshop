08e67da764d57e2d65528053fe4f23b8290fab27
wi1helm <wilhelm.lidbladjerrestal@elev.ga.ntig.se>
2023-02-16 15:58:18 +0100

    java scripts ej fungerande

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+//const img1 = {"id":"anglewar","title":"Angle War 4k","desc":"Obama","price":"100"}
+
+function changeImage(){
+    document.getElementById("img1").src="../images/new/angle war.png";
+    console.log("obama");
+    
+}
