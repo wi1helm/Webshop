985fcdbdf85069e3aa8c04dfb9984b2a43c46626
wi1helm <wilhelm.lidbladjerrestal@elev.ga.ntig.se>
2023-02-17 14:02:19 +0100

    Added images

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 
-let products = [{id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
-                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
-                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
-                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
-                {id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"100Kr",img:"../images/new/angle war.png"},
+let products = [{id:"angle_war",title:"Angle War",theme:"Fantasy",decs:"Obama",price:"120Kr",img:"../images/new/angle war.png"},
+                {id:"mount_resv",title:"Mountain Reserve",theme:"Nature",decs:"Obama",price:"160Kr",img:"../images/new/Montain Reserve.png"},
+                {id:"wavy_fall",title:"Wavy Waterfall",theme:"Nature",decs:"Obama",price:"99Kr",img:"../images/new/wavy waterfall.png"},
+                {id:"world_prot",title:"World Protest",theme:"Sci-Fi",decs:"Obama",price:"149Kr",img:"../images/new/world protest.png"},
+                {id:"ethereal_sunrise",title:"Ethereal Sunrise",theme:"Fantasy",decs:"Obama",price:"99Kr",img:"../images/low/Ethereal Sunrise.png"},
 
 ]
 
@@ -15,6 +15,9 @@ function saveClick(id){
 
 function load(){
     lastclick = sessionStorage.getItem("lastClick");
+    if (lastclick == undefined){
+        location.href = "../index.html";
+    }
     console.log(lastclick);
     document.getElementById("title").innerHTML = products.find(x => x.id === lastclick).title;
     document.getElementById("theme").innerHTML = products.find(x => x.id === lastclick).theme;
